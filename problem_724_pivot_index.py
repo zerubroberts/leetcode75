0a50c3de919ebc2c3a7f6af4d56d5381859f2cf2
Zerub Roberts <zrmuthya@deakin.edu.au>
2022-11-24 17:46:05 +1100

    Solve Problem 724 - Pivot Index

--- problem_724_pivot_index.py
+++ problem_724_pivot_index.py
@@ -1,14 +1,38 @@
+
 from typing import List
 
 class Solution:
     def pivotIndex(self, nums:List[int]) -> int:
-        myvar = 23
-        return 4402
+
+        #create list with cumulative sum
+        nums_cumsum = []
+        counter=0
+        for i,n in enumerate(nums):
+            counter = counter+n
+            nums_cumsum.append(counter)
+
+        #same as above - but reverse
+        nums_cumsum_reverse = []
+        counter=0
+        for i,n in enumerate(nums[::-1]):
+            counter = counter+n
+            nums_cumsum_reverse.append(counter)
+        
+        #reverse it again
+        nums_cumsum_reverse = nums_cumsum_reverse[::-1]
+
+        #find the index where the two sums match
+        for i,j in enumerate(nums_cumsum):
+            if nums_cumsum[i] == nums_cumsum_reverse[i]:
+                return i
+                break
+        else:
+            return -1
 
 
 
 myclass = Solution()
-print(myclass.pivotIndex([3,4]))
+print(myclass.pivotIndex([1,7,3,6,5,6]))
 
 if __name__ == '__main__':
     pass
--- test_724_pivot_index.py
+++ test_724_pivot_index.py
@@ -3,10 +3,16 @@ import unittest   # The test framework
 
 class Test_LeetCode(unittest.TestCase):
     def test_case1(self):
-        assert Solution.pivotIndex(self, [43,44]) == 44
+        assert Solution.pivotIndex(self, [1,7,3,6,5,6]) == 3
 
     def test_case2(self):
-        assert Solution.pivotIndex(self, [143,44]) == 4402
+        assert Solution.pivotIndex(self,  [1,2,3]) == -1
+
+    def test_case3(self):
+        assert Solution.pivotIndex(self,  [2,1,-1]) == 0
+
+
+
 
 
 
